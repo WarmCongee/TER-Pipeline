de7647017c0f9fe932a18f3280482055097e6222
Yuzhe WENG <warmcongee@gmail.com>
2023-05-08 23:39:18 +0800

    Complete text dataset construction

---
+++ config_path.py
@@ -0,0 +1,17 @@
+
+# *_*coding:utf-8 *_*
+
+# Original dataset path
+MER_DATASET_PATH_SOURCE = "/home/dataset/wyz_mer_dataset/mer_dataset_source/"
+MER_DATASET_PATH_SOURCE_TRAIN = "/home/dataset/wyz_mer_dataset/mer_dataset_source/train/"
+
+
+# Processed dataset path
+TER_DATASET_PATH = "/home/dataset/wyz_mer_dataset/ter_dataset/"
+TER_DATASET_PATH_VIDEO = "/home/dataset/wyz_mer_dataset/ter_dataset/video/"
+TER_DATASET_PATH_AUDIO = "/home/dataset/wyz_mer_dataset/ter_dataset/audio/"
+TRAIN_LABLE_NPZ_PATH = "/home/dataset/wyz_mer_dataset/ter_dataset/label-6way.npz"
+ASR_TRANS_DATASET_PATH = "/home/dataset/wyz_mer_dataset/ter_dataset/base_text_dataset_all.csv"
+
+# Tools path
+PATH_TO_FFMPEG = '/home/wyz/MER-TER/TER-Pipeline/tools/ffmpeg-4.4.1-i686-static/ffmpeg'
--- text_dataset_extract.py
+++ text_dataset_extract.py
@@ -16,7 +16,7 @@ import numpy as np
 import pandas as pd
 
 import cv2 # pip install opencv-python
-import config
+import config_path
 
 # split audios from videos
 def split_audio_from_video_16k(video_root, save_root):
@@ -25,7 +25,7 @@ def split_audio_from_video_16k(video_root, save_root):
         videoname = os.path.basename(video_path)[:-4]
         audio_path = os.path.join(save_root, videoname + '.wav')
         if os.path.exists(audio_path): continue
-        cmd = "%s -loglevel quiet -y -i %s -ar 16000 -ac 1 %s" %(config.PATH_TO_FFMPEG, video_path, audio_path)
+        cmd = "%s -loglevel quiet -y -i %s -ar 16000 -ac 1 %s" %(config_path.PATH_TO_FFMPEG, video_path, audio_path)
         os.system(cmd)
 
 # preprocess dataset-release
@@ -33,9 +33,6 @@ def normalize_dataset_format(data_root, save_root):
     ## input path
     train_data  = os.path.join(data_root, 'train')
     train_label = os.path.join(data_root, 'train-label.csv')
-    test1_data  = os.path.join(data_root, 'test1')
-    test2_data  = os.path.join(data_root, 'test2')
-    test3_data  = os.path.join(data_root, 'test3')
 
     ## output path
     save_video = os.path.join(save_root, 'video')
@@ -44,7 +41,7 @@ def normalize_dataset_format(data_root, save_root):
     if not os.path.exists(save_video): os.makedirs(save_video)
 
     ## move all videos into save_video
-    for temp_root in [train_data, test1_data, test2_data, test3_data]:
+    for temp_root in [train_data]:
         video_paths = glob.glob(temp_root + '/*')
         for video_path in tqdm.tqdm(video_paths):
             video_name = os.path.basename(video_path)
@@ -60,48 +57,39 @@ def normalize_dataset_format(data_root, save_root):
         val  = row['valence']
         train_corpus[name] = {'emo': emo, 'val': val}
 
-    test1_corpus = {}
-    for video_path in glob.glob(test1_data + '/*'):
-        video_name = os.path.basename(video_path)[:-4]
-        test1_corpus[video_name] = {'emo': 'neutral', 'val': -10} # for convenience
-
-    test2_corpus = {}
-    for video_path in glob.glob(test2_data + '/*'):
-        video_name = os.path.basename(video_path)[:-4]
-        test2_corpus[video_name] = {'emo': 'neutral', 'val': -10} # for convenience
-
-    test3_corpus = {}
-    for video_path in glob.glob(test3_data + '/*'):
-        video_name = os.path.basename(video_path)[:-4]
-        test3_corpus[video_name] = {'emo': 'neutral', 'val': -10} # for convenience
-
     np.savez_compressed(save_label,
-                        train_corpus=train_corpus,
-                        test1_corpus=test1_corpus,
-                        test2_corpus=test2_corpus,
-                        test3_corpus=test3_corpus)
+                        train_corpus=train_corpus)
 
 # generate transcription files using asr
-def generate_transcription_files_asr(audio_root, save_path):
+def generate_transcription_files_asr(audio_root: str, save_path: str):
     import torch
     import wenetruntime as wenet
-    decoder = wenet.Decoder('./tools/wenet/wenetspeech_u2pp_conformer_libtorch', lang='chs')
-
+    decoder = wenet.Decoder("/home/wyz/MER-TER/TER-Pipeline/tools/wenet/wenetspeech_u2pp_conformer_libtorch", lang='chs')
+    
     names = []
     sentences = []
+    emos =[]
+    valences =[]
+
+    lable_dictionary = np.load(config_path.TRAIN_LABLE_NPZ_PATH, allow_pickle=True)
+
     for audio_path in tqdm.tqdm(glob.glob(audio_root + '/*')):
         name = os.path.basename(audio_path)[:-4]
+        print(audio_path)
         sentence = decoder.decode_wav(audio_path)
         sentence = sentence.split('"')[5]
         names.append(name)
         sentences.append(sentence)
+        
+        emos.append(lable_dictionary['train_corpus'][()][name]['emo'])
+        valences.append(lable_dictionary['train_corpus'][()][name]['val'])
 
     ## write to csv file
-    columns = ['name', 'sentence']
-    data = np.column_stack([names, sentences])
+    columns = ['emo', 'val', 'name', 'sentence']
+    data = np.column_stack([emos, valences, names, sentences])
     df = pd.DataFrame(data=data, columns=columns)
     df[columns] = df[columns].astype(str)
-    df.to_csv(save_path, index=False)
+    df.to_csv(save_path, sep='\t', index=False)
 
 # add punctuation to transcripts
 def refinement_transcription_files_asr(old_path, new_path):
@@ -128,6 +116,13 @@ def refinement_transcription_files_asr(old_path, new_path):
     df[columns] = df[columns].astype(str)
     df.to_csv(new_path, index=False)
 
+def construct_text_dataset():
+    #normalize_dataset_format(config_path.MER_DATASET_PATH_SOURCE, config_path.TER_DATASET_PATH)
+    #split_audio_from_video_16k(config_path.TER_DATASET_PATH_VIDEO, config_path.TER_DATASET_PATH_AUDIO)
+    generate_transcription_files_asr(config_path.TER_DATASET_PATH_AUDIO, config_path.ASR_TRANS_DATASET_PATH)
+
+
 if __name__ == '__main__':
-    import fire
-    fire.Fire()
+    construct_text_dataset()
+    # import fire
+    # fire.Fire()
